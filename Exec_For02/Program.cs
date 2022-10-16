df4fde3268e20c177bff25c38c7492cf0600e101
w2192 <w2192@LAPTOP-2BO0FNQF>
2022-10-16 23:45:53 +0800

    complete

---
+++ Exec_For02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_For02
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			for (int i = 1; i <= 9; i += 2) 
+			{
+				int countOfStar = i;
+				int countOfEmpty = (9 - i)/2;
+
+				string row = new string(' ',countOfEmpty)+new string('*',countOfStar)+new string(' ',countOfEmpty);
+				Console.WriteLine(row);
+			}
+		}
+	}
+}
